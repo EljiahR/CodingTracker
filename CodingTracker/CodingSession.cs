8dc0df1bf3e2992e745d3b8459e97a51d6b0f4c8
Elijah Reck <kcerhajile@gmail.com>
2024-05-19 14:54:10 -0400

    Set up CodingSession class with duration calculator method

---
+++ CodingTracker/CodingSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    internal class CodingSession
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan CalculateDuration()
+        {
+            return EndTime.Subtract(StartTime);
+        }
+    }
+}
--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodingTracker;
+
+UserInput userInput = new UserInput();
+userInput.Test();
+
+CodingSession session = new CodingSession();
+session.StartTime = DateTime.Now;
+Console.WriteLine("Start: " +  session.StartTime.ToString("hh:mm:ss"));
+Console.ReadLine();
+session.EndTime = DateTime.Now;
+Console.WriteLine("End: " + session.EndTime.ToString("hh:mm:ss"));
+Console.WriteLine("Elapsed time: " + session.CalculateDuration());
+
