6ec4542fc570aa78b85753ecbbe493d0bf530bbc
Elijah <kcerhajile@gmail.com>
2024-08-25 17:14:27 -0400

    Deleted test project because it didnt work for some reason, new project works even though the code was copy/pasted

--- CodingTracker.Test/ValidationTests.cs
+++ CodingTracker.Test/ValidationTests.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using CodingTracker;
 
 namespace CodingTracker.Test
 {
     [TestClass]
     public class ValidationTests
     {
+        // For menu input validation tests
+        int start = 0;
+        int end = 7;
         [TestMethod]
-        public void TestMethod1()
+        public void InputValidation_ValidateMenuOption_InputInRange_ReturnTrue()
         {
+            bool result = InputValidation.ValidateMenuOption(start, end, "3");
         }
     }
 }
---
+++ CodingTrackerApp.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CodingTracker;
+
+namespace CodingTracker.Test
+{
+	[TestClass]
+	public class ValidationTests
+	{
+		// For menu input validation tests
+		int start = 0;
+		int end = 7;
+		[TestMethod]
+		public void InputValidation_ValidateMenuOption_InputInRange_ReturnTrue()
+		{
+			bool result = InputValidation.ValidateMenuOption(start, end, "5");
+			Assert.IsTrue(result);
+		}
+	}
+}
